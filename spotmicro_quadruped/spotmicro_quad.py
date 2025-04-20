d49be23fb3f954882cb387eeb2dc6aceaef2bffb
tonhathuy <tonhathuy97@gmail.com>
2025-04-20 12:31:47 +0000

    add ImplicitActuatorCfg for actuators

--- spotmicro_quadruped/spotmicro_quad.py
+++ spotmicro_quadruped/spotmicro_quad.py
@@ -1,5 +1,5 @@
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import ActuatorNetMLPCfg, DCMotorCfg, ImplicitActuatorCfg, IdealActuatorCfg
+from isaaclab.actuators import ActuatorNetMLPCfg, DCMotorCfg, ImplicitActuatorCfg, IdealPDActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 import os
 from math import pi
@@ -35,12 +35,20 @@ SPOTMICRO_QUAD_CFG = ArticulationCfg(
     ),
     soft_joint_pos_limit_factor=0.9,
     actuators={
-        "base_legs": IdealActuatorCfg(
+        "base_legs": IdealPDActuatorCfg(
             joint_names_expr=[".*_leg", ".*_foot", ".*_shoulder"],
             stiffness    = 3.4 / 0.35,        # ≈ 10 N·m/rad  → có thể nâng tới ~40 khi robot nặng
             damping      = 0.05 * 40.0,       # ≈ 2 N·m·s/rad  (5 % k‑p) – bắt đầu ít, tăng dần
             effort_limit = 3.5,               # N·m  (ngang stall‑torque 6 V)
             velocity_limit = 8.0              # rad/s (> 7.5 để không kìm tốc độ tối đa)
         ),
+        # "base_legs": ImplicitActuatorCfg(
+        #     joint_names_expr=[".*_leg", ".*_foot", ".*_shoulder"],   # regex khớp tên hinge
+        #     stiffness=20.0,                    # k_p  (N·m/rad)
+        #     damping=0.5,                       # k_d  (N·m·s/rad)
+        #     effort_limit=3.4,                  # τ_max
+        #     velocity_limit=8.0,                # |ω|
+        #     armature=0.0,                      # giữ mặc định
+        # )
     },
 )
